7025bf565ff504b597757767c85be18b2e92b812
Chupa <amalik.moussaoui1710@gmail.com>
2026-02-11 14:20:08 +0100

    feat: add MovieCarousel component to Home page for enhanced movie display

--- frontend-user/src/pages/Home.jsx
+++ frontend-user/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Navbar from '../components/common/Navbar';
 import MovieHero from '../components/movies/MovieHero';
 import MovieList from '../components/movies/MovieList';
+import MovieCarousel from '../components/movies/MovieCarousel';
 import Footer from '../components/layout/Footer';
 import moviesData from '../data/movies.json';
 import MovieFilter from '../components/movies/MovieFilter';
@@ -16,6 +17,7 @@ function Home() {
             <Navbar />
             <MovieHero movie={featuredMovie} />
             <div className="-mt-15 relative z-10 space-y-4 pb-12">
+                <MovieCarousel title="Tendances Actuelles" movies={moviesData} />
                 <MovieFilter movies={moviesData} onFilter={setFilteredMovies} />
                 <MovieList title="Catalogue" movies={filteredMovies} />
             </div>
